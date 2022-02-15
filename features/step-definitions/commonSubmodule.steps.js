de19351c10d296a0d0dd2a8957d5503f36d1644c
Ivan <80200619+Ivan643@users.noreply.github.com>
2022-02-15 15:09:40 +0300

    Delete features directory

--- features/step-definitions/commonSubmodule.steps.js
+++
@@ -1,8 +0,0 @@
-const { When } = require('@wdio/cucumber-framework');
-const commonSubmodulePage = require('../pageobjects/commonSubmodule.page');
-
-When(/^I click on the "(.*)" submodule item$/, async (text) => {
-    await commonSubmodulePage.submoduleItem(text).waitForDisplayed();
-    await commonSubmodulePage.submoduleItem(text).scrollIntoView();
-    await commonSubmodulePage.submoduleItem(text).click();
-});
